b469fb1a34d7eca31ff002a34ce9f8eb21cbef5d
Siboniso <sibandzesiboniso0@gmail.com>
2025-07-06 17:51:41 +0200

    Refactor capital history report for improved readability and consistency in variable initialization

--- reports/capitalhistory.php
+++ reports/capitalhistory.php
@@ -1,89 +1,74 @@
 <?php
 require_once '../scripts/connection.php';
-$ii = $_POST['single'];
-	 $d1=$_POST['date1'];
-	 $d2=$_POST['date2'];
 
+$ii = $_POST['single'] ?? [];
+$d1 = $_POST['date1'] ?? '';
+$d2 = $_POST['date2'] ?? '';
 
-foreach ($ii as $a){
-$mntharray[] = $a;
+$mntharray = [];
+foreach ($ii as $a) {
+    $mntharray[] = $a;
 }
 $mntharray1 = json_encode($mntharray);
-$mntharray2 =  str_replace( array('[',']') , ''  , $mntharray1 );
+$mntharray2 = str_replace(['[', ']'], '', $mntharray1);
 
-//echo $mntharray1;
-$name = array($mntharray2);
-
-if (in_array("all", $mntharray)){
- // echo $mntharray2;
-  //console("HHHH");
-   $choose = "`transaction_type1_view` WHERE DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC";  
-}else{
-  
- $choose = "`transaction_type1_view` WHERE MemberNo IN ({$mntharray2}) AND DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC";   
+if (in_array("all", $mntharray)) {
+    $choose = "`transaction_type1_view` WHERE DATE(TransactionDate) BETWEEN '$d1' AND '$d2' ORDER BY TransactionDate DESC";
+} else {
+    $choose = "`transaction_type1_view` WHERE MemberNo IN ({$mntharray2}) AND DATE(TransactionDate) BETWEEN '$d1' AND '$d2' ORDER BY TransactionDate DESC";
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $sql = "SELECT MemberNo, Name, Surname, DOB, Gender, TransactionDate, `Amount Paid` AS AmountPaid FROM $choose";
+    $stmt12 = $conn->prepare($sql);
 
+    if ($stmt12 && $stmt12->execute()) {
+        $result12 = $stmt12->get_result();
 
-
-
-if(count($_POST)>0){
-$stmt12 = $conn->prepare("SELECT MemberNo, Name, Surname, DOB, Gender, TransactionDate, Amount Paid FROM " . $choose);
-						$stmt12->execute();
-						$result12 = $stmt12->get_result();
-						if ($result12->num_rows > 0) {
-						  // output data of each row
-						 ?>
-						 <div class="table-responsive">
-		<table class="table datatable"  id="free">
-			<thead>
-                  <tr>
-                    <th scope="col">Member No</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Surname</th>
-                    <th scope="col">DOB</th>
-                    <th scope="col">Gender</th>
-                    <th scope="col">Transaction Date</th>
-                    <th scope="col">Amount Paid</th>
-                  </tr>
-                </thead>
-                <tbody>
-				
-          
-								
-<tr>
-     <th scope="row"><?php echo $memberID; ?></th>
-                    <th scope="row"><?php echo $row12['TransactionDate']; ?></th>
-                    <td><?php echo $row12['Details']; ?></td>
-                    <td><?php echo $tyes; ?></td>
-                    <td><?php echo $row12['Comments']; ?></td>
-                    <td><?php echo number_format($row12['StartingBalance'], 2); ?></td>
-					<td><?php echo  number_format($row12['Amount'], 2);  ?></td>
-					<td><?php echo number_format($row12['NewBalance'], 2); ?></td>
-					<td>
-			  
-					</td>
-                  </tr>
-				   
-<?php						
-						}
-						?>
-						</tbody>
-						 </table>
-						</div>
-						<?php
-						
-						} else {
-						  echo "0 results";
-
-
-
-						} 
+        if ($result12->num_rows > 0) {
 ?>
-  <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
-
-<?php						
-
-
-
-
+<div class="table-responsive">
+    <table class="table datatable" id="free">
+        <thead>
+            <tr>
+                <th>Member No</th>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>DOB</th>
+                <th>Gender</th>
+                <th>Transaction Date</th>
+                <th>Amount Paid</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+            while ($row12 = $result12->fetch_assoc()) {
+?>
+            <tr>
+                <td><?php echo htmlspecialchars($row12['MemberNo']); ?></td>
+                <td><?php echo htmlspecialchars($row12['Name']); ?></td>
+                <td><?php echo htmlspecialchars($row12['Surname']); ?></td>
+                <td><?php echo htmlspecialchars($row12['DOB']); ?></td>
+                <td><?php echo htmlspecialchars($row12['Gender']); ?></td>
+                <td><?php echo htmlspecialchars($row12['TransactionDate']); ?></td>
+                <td><?php echo number_format($row12['AmountPaid'], 2); ?></td>
+            </tr>
+<?php
+            }
+?>
+        </tbody>
+    </table>
+</div>
+<script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
+<?php
+        } else {
+            echo "0 results";
+        }
+    } else {
+        echo "Failed to execute query.";
+    }
+} else {
+    header('Location: ./');
+    exit();
+}
+?>
