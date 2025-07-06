b5bc4a3e1967b9e75e1a41616d99436f40a16ddb
Siboniso <sibandzesiboniso0@gmail.com>
2025-07-06 20:21:51 +0200

    Refactor consolhistory.php for improved security and performance by using prepared statements and optimizing SQL queries.

--- membership/consolhistory.php
+++ membership/consolhistory.php
@@ -1,43 +1,43 @@
 <?php
 require_once '../scripts/connection.php';
+
 $ff = $_POST['c_id'] ?? '';
 $d1 = $_POST['from'] ?? '';
 $d2 = $_POST['to'] ?? '';
 
-if (count($_POST) > 0) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmtz = $conn->prepare("
-    SELECT
-      m.MemberID,
-      m.MemberNo,
-      m.MemberSurname,
-      m.MemberFirstname,
-      m.MemberIDnumber,
-      m.DeceasedID,
-      m.Gender,
-      m.DateOfBirth,
-      m.ApprovedBenefit,
-      m.DateAccountOpened,
-      m.Terminated,
-      m.MemberPostalAddress,
-      d.RetirementFundID
-    FROM
-      `tblmembers` AS m
-    JOIN
-      `tbldeceased` AS d ON m.DeceasedID = d.DeceasedID
-    JOIN
-      `tblmemberaccounts` AS ma ON m.MemberID = ma.memberID
-    WHERE
-      ma.TransactionDate BETWEEN '$d1' AND '$d2'
-      AND d.RetirementFundID = '$ff'
-    GROUP BY
-      m.MemberID;
-     ");
+        SELECT
+            m.MemberID,
+            m.MemberNo,
+            m.MemberSurname,
+            m.MemberFirstname,
+            m.MemberIDnumber,
+            m.DeceasedID,
+            m.Gender,
+            m.DateOfBirth,
+            m.ApprovedBenefit,
+            m.DateAccountOpened,
+            m.Terminated,
+            m.MemberPostalAddress,
+            d.RetirementFundID
+        FROM tblmembers AS m
+        JOIN tbldeceased AS d ON m.DeceasedID = d.DeceasedID
+        JOIN tblmemberaccounts AS ma ON m.MemberID = ma.memberID
+        WHERE ma.TransactionDate BETWEEN ? AND ?
+          AND d.RetirementFundID = ?
+        GROUP BY m.MemberID
+    ");
+    $stmtz->bind_param("ssi", $d1, $d2, $ff);
     $stmtz->execute();
     $resultz = $stmtz->get_result();
+
     if ($resultz->num_rows > 0) {
         while ($rowz = $resultz->fetch_assoc()) {
             $ii = $rowz['MemberID'];
-?>
+
+            // --- Start HTML output per member
+            ?>
             <div class="table-responsive">
                 <table class="table datatable" id="free">
                     <thead>
@@ -59,6 +59,14 @@ if (count($_POST) > 0) {
                         </tr>
                     </thead>
                     <tbody>
+                        <?php
+                        // Get Financials
+                        $initAmt = getAmount($conn, "SELECT StartingBalance FROM tblmemberaccounts WHERE memberID=? AND TransactionDate>=? ORDER BY TransactionDate ASC LIMIT 1", "is", [$ii, $d1], 'StartingBalance');
+                        $interest = getAmount($conn, "SELECT SUM(Amount) AS sumAmt FROM tblmemberaccounts WHERE memberID=? AND TransactionTypeID=8 AND TransactionDate BETWEEN ? AND ?", "iss", [$ii, $d1, $d2], 'sumAmt');
+                        $expenses = getAmount($conn, "SELECT SUM(Amount) AS sumAmt FROM tblmemberaccounts WHERE memberID=? AND TransactionTypeID IN (2,5,6,7) AND TransactionDate BETWEEN ? AND ?", "iss", [$ii, $d1, $d2], 'sumAmt');
+                        $payments = getAmount($conn, "SELECT SUM(Amount) AS sumAmt FROM tblmemberaccounts WHERE memberID=? AND TransactionTypeID IN (3,4) AND TransactionDate BETWEEN ? AND ?", "iss", [$ii, $d1, $d2], 'sumAmt');
+                        $latestBal = getAmount($conn, "SELECT NewBalance FROM tblmemberaccounts WHERE memberID=? AND TransactionDate<=? ORDER BY TransactionDate DESC LIMIT 1", "is", [$ii, $d2], 'NewBalance');
+                        ?>
                         <tr>
                             <td><?php echo htmlspecialchars($rowz['MemberNo']); ?></td>
                             <td><?php echo htmlspecialchars($rowz['MemberFirstname']); ?></td>
@@ -66,154 +74,50 @@ if (count($_POST) > 0) {
                             <td><?php echo htmlspecialchars($rowz['DateOfBirth']); ?></td>
                             <td><?php echo htmlspecialchars($rowz['Gender']); ?></td>
                             <td><?php echo htmlspecialchars($rowz['DateAccountOpened']); ?></td>
-                            <?php
-                            // initial amount at period start
-                            $stmtInit = $conn->prepare(
-                                "SELECT StartingBalance 
-                                   FROM tblmemberaccounts 
-                                   WHERE memberID=? AND TransactionDate>=? 
-                                   ORDER BY TransactionDate ASC LIMIT 1"
-                            );
-                            $stmtInit->bind_param("is", $ii, $d1);
-                            $stmtInit->execute();
-                            $rInit = $stmtInit->get_result();
-                            $initAmt = $rInit->num_rows ? $rInit->fetch_assoc()['StartingBalance'] : 0;
-
-                            // interest (TransactionTypeID=8)
-                            $stmtInt = $conn->prepare(
-                                "SELECT SUM(Amount) AS sumAmt 
-                                   FROM tblmemberaccounts 
-                                   WHERE memberID=? AND TransactionTypeID=8 
-                                     AND TransactionDate BETWEEN ? AND ?"
-                            );
-                            $stmtInt->bind_param("iss", $ii, $d1, $d2);
-                            $stmtInt->execute();
-                            $rInt = $stmtInt->get_result();
-                            $interest = $rInt->fetch_assoc()['sumAmt'] ?? 0;
-
-                            // expenses (2,5,6,7)
-                            $stmtExp = $conn->prepare(
-                                "SELECT SUM(Amount) AS sumAmt 
-                                   FROM tblmemberaccounts 
-                                   WHERE memberID=? AND TransactionTypeID IN (2,5,6,7) 
-                                     AND TransactionDate BETWEEN ? AND ?"
-                            );
-                            $stmtExp->bind_param("iss", $ii, $d1, $d2);
-                            $stmtExp->execute();
-                            $rExp = $stmtExp->get_result();
-                            $expenses = $rExp->fetch_assoc()['sumAmt'] ?? 0;
-
-                            // payments (3,4)
-                            $stmtPay = $conn->prepare(
-                                "SELECT SUM(Amount) AS sumAmt 
-                                   FROM tblmemberaccounts 
-                                   WHERE memberID=? AND TransactionTypeID IN (3,4) 
-                                     AND TransactionDate BETWEEN ? AND ?"
-                            );
-                            $stmtPay->bind_param("iss", $ii, $d1, $d2);
-                            $stmtPay->execute();
-                            $rPay = $stmtPay->get_result();
-                            $payments = $rPay->fetch_assoc()['sumAmt'] ?? 0;
-
-                            // latest balance at period end
-                            $stmtBal = $conn->prepare(
-                                "SELECT NewBalance 
-                                   FROM tblmemberaccounts 
-                                   WHERE memberID=? AND TransactionDate<=? 
-                                   ORDER BY TransactionDate DESC LIMIT 1"
-                            );
-                            $stmtBal->bind_param("is", $ii, $d2);
-                            $stmtBal->execute();
-                            $rBal = $stmtBal->get_result();
-                            $latestBal = $rBal->num_rows ? $rBal->fetch_assoc()['NewBalance'] : 0;
-                            ?>
                             <td><?php echo number_format($initAmt, 2); ?></td>
                             <td><?php echo number_format($interest, 2); ?></td>
                             <td><?php echo number_format($expenses, 2); ?></td>
                             <td><?php echo number_format($payments, 2); ?></td>
                             <td><?php echo number_format($latestBal, 2); ?></td>
                         </tr>
+
+                        <!-- Extra Sections: Expenses, Payments, Others -->
+                        <?php
+                        $expTotal = getAmount($conn, "SELECT SUM(Amount) AS TT3 FROM tblmemberaccounts WHERE TransactionTypeID IN (3,4) AND TransactionDate BETWEEN ? AND ? AND memberID = ?", "ssi", [$d1, $d2, $ii], 'TT3');
+                        $otherTotal = getAmount($conn, "SELECT SUM(Amount) AS TT3 FROM tblmemberaccounts WHERE TransactionTypeID = 10 AND TransactionDate BETWEEN ? AND ? AND memberID = ?", "ssi", [$d1, $d2, $ii], 'TT3');
+                        ?>
+                        <tr>
+                            <th colspan="6" style="text-align:right;">Payments Total:</th>
+                            <td colspan="5"><?php echo "- E " . number_format($expTotal, 2); ?></td>
+                        </tr>
+                        <tr>
+                            <th colspan="6" style="text-align:right;">Other Transactions:</th>
+                            <td colspan="5"><?php echo "E " . number_format($otherTotal, 2); ?></td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
-<?php
-        }  // end while $rowz
+            <?php
+        } // end while
     } else {
-        echo "0 results";
+        echo "0 results found.";
     }
 } else {
-    header('location: ./');
-    exit();
+    header("Location: ./");
+    exit;
+}
+
+// Helper function to run and fetch a single value
+function getAmount($conn, $sql, $types, $params, $field)
+{
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param($types, ...$params);
+    $stmt->execute();
+    $res = $stmt->get_result();
+    if ($res->num_rows > 0) {
+        return $res->fetch_assoc()[$field] ?? 0;
+    }
+    return 0;
 }
 ?>
-                  
-                   
-                    <tr>
-                    <th scope="col" colspan="3" style="vertical-align: top;">Expenses/Costs</th>
-                    <td scope="col" colspan="3" style="text-align: right;"><?php echo "- E ". number_format($row12['TT3'], 2); ?></td>
-                    </tr>
-
- <?php	}
-	?>
-
-<?php	} else {
-echo "0 results";	}    
- 
- 
- 
- 
-     
-$stmt12 = $conn->prepare("SELECT SUM(`Amount`) AS `TT3` from `tblmemberaccounts` where  TransactionTypeID IN ('3', '4') 
-AND (`TransactionDate` BETWEEN '$d1' AND '$d2') AND memberID = '$ii' ");
-						$stmt12->execute();
-						$result12 = $stmt12->get_result();
-						if ($result12->num_rows > 0) {
-						    while($row12 = $result12->fetch_assoc()) {
-						  // output data of each row
-						 // $sum = $sum + $row12['TT3'] ;
-						 ?>
-	
-                  
-                   
-                    <tr>
-                    <th scope="col" colspan="3" style="vertical-align: top;">Payments</th>
-                    <td scope="col" colspan="3" style="text-align: right;"><?php echo "- E ".number_format($row12['TT3'], 2); ?></td>
-                    </tr>
-
- <?php	}
-	?>
-
-<?php	} else {
-echo "0 results";	}    
- 
- 
-     
-$stmt12 = $conn->prepare("SELECT SUM(`Amount`) AS `TT3` from `tblmemberaccounts` where  TransactionTypeID = '10' AND (`TransactionDate` BETWEEN '$d1' AND '$d2') AND memberID = '$ii' ");
-						$stmt12->execute();
-						$result12 = $stmt12->get_result();
-						if ($result12->num_rows > 0) {
-						    while($row12 = $result12->fetch_assoc()) {
-						  // output data of each row
-						 // $sum = $sum + $row12['TT3'] ;
-						 ?>
-	
-                   
-                    <tr>
-                    <th scope="col" colspan="3" style="vertical-align: top;">Other Transactions</th>
-                    <td scope="col" colspan="3" style="text-align: right;"><?php echo "E ".number_format($row12['TT3'], 2); ?></td>
-                    </tr>
-</thead>
- <?php	}
-	?>
-</table>
-</div>
-<?php	} else {
-echo "0 results";	}    
- 
-?>
-  <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
-
-<?php						
-
-
-
+<script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
