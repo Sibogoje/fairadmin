5a184d78b1753294917978bde67e1b95bddcaa4f
Siboniso <sibandzesiboniso0@gmail.com>
2025-07-06 09:13:52 +0200

    Update dashboard title to "Dashboard Test"

--- dash.php
+++ dash.php
@@ -56,7 +56,7 @@ include 'scripts/connection.php';
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Dashboard</h1>
+      <h1>Dashboard Test</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="dash.php">Home</a></li>
