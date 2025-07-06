42bfbbd999daf1ecc4b37a72c1598c0414e0ff3b
Siboniso <sibandzesiboniso0@gmail.com>
2025-07-06 20:07:36 +0200

    Refactor consolhistory.php for improved data presentation and security by using prepared statements and HTML escaping

--- membership/consolhistory.php
+++ membership/consolhistory.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once '../scripts/connection.php';
 $ff = $_POST['c_id'];
@@ -39,8 +38,7 @@ GROUP BY
 						$stmtz->execute();
 						$resultz = $stmtz->get_result();
 						if ($resultz->num_rows > 0) {
-						    while($rowz = $resultz->fetch_assoc()) {   
-    
+						    while($rowz = $resultz->fetch_assoc()) {
     $ii = $rowz['MemberID'];
         
 
@@ -48,99 +46,101 @@ GROUP BY
 						 <div class="table-responsive">
 		<table class="table datatable"  id="free">
 			<thead>
-                  
-                   	<tr style="text-align: center; background: black; color: white;">
-                    <th scope="col" colspan="6"><?php echo $rowz['MemberFirstname']." ".$rowz['MemberSurname']; ?></th>
-                   
-                    </tr>
-                    <tr>
-                    <th scope="col" style="vertical-align: top;">FundID</th>
-                    <td scope="col"><?php echo $rowz['RetirementFundID']; ?></td>
-                    <th scope="col" style="vertical-align: top;">MemberNo</th>
-					<td scope="col"><?php echo $rowz['MemberNo']; ?></td>
-					<th scope="col" style="vertical-align: top;">Gender</th>
-					<td scope="col"><?php echo $rowz['Gender']; ?></td>
-					</tr>
-					
-					
-					<tr>
-                    <th scope="col" style="vertical-align: top;">Date of Birth</th>
-                    <td scope="col"><?php echo $rowz['DateOfBirth']; ?></td>
-                    <th scope="col" style="vertical-align: top;">Account Opened</th>
-					<td scope="col"><?php echo $rowz['DateAccountOpened']; ?></td>
-					 <th scope="col" style="vertical-align: top;">Postal Address</th>
-					<td scope="col"><?php echo $rowz['MemberPostalAddress']; ?></td>
-					</tr>
-					
-						<tr>
-                    <th scope="col" style="vertical-align: top;">Approved Benefit</th>
-                    <td scope="col"  style="font-weight: bold;"><?php echo "E ". number_format($rowz['ApprovedBenefit'], 2); ?></td>
-                    <th scope="col" style="vertical-align: top;">Terminated</th>
-					<td scope="col"><?php echo $rowz['Terminated']; ?></td>
-					 <th scope="col" style="vertical-align: top;">Balance</th>
+      <tr style="text-align:center; background:black; color:white;">
+        <th colspan="11"><?php echo htmlspecialchars($rowz['MemberFirstname']." ".$rowz['MemberSurname']); ?></th>
+      </tr>
+      <tr>
+        <th>Member No.</th><th>Name</th><th>Surname</th><th>DOB</th><th>Gender</th>
+        <th>Date of Joining</th><th>Initial Amount</th><th>Interest</th>
+        <th>Expenses</th><th>Payments</th><th>Latest Balance</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><?php echo htmlspecialchars($rowz['MemberNo']); ?></td>
+        <td><?php echo htmlspecialchars($rowz['MemberFirstname']); ?></td>
+        <td><?php echo htmlspecialchars($rowz['MemberSurname']); ?></td>
+        <td><?php echo htmlspecialchars($rowz['DateOfBirth']); ?></td>
+        <td><?php echo htmlspecialchars($rowz['Gender']); ?></td>
+        <td><?php echo htmlspecialchars($rowz['DateAccountOpened']); ?></td>
+        <?php
+        // initial amount at period start
+        $stmtInit = $conn->prepare(
+          "SELECT StartingBalance 
+           FROM tblmemberaccounts 
+           WHERE memberID=? AND TransactionDate>=? 
+           ORDER BY TransactionDate ASC LIMIT 1"
+        );
+        $stmtInit->bind_param("is",$ii,$d1);
+        $stmtInit->execute();
+        $rInit = $stmtInit->get_result();
+        $initAmt = $rInit->num_rows ? $rInit->fetch_assoc()['StartingBalance'] : 0;
 
-					 <?php
-					 $stmt12 = $conn->prepare("SELECT `NewBalance` from `balances` where  `memberID` = '$ii' ");
-						$stmt12->execute();
-						$result12 = $stmt12->get_result();
-						if ($result12->num_rows > 0) {
-						    while($row12 = $result12->fetch_assoc()) {
-?>
-			<td scope="col" style="font-weight: bold;"><?php echo "E ". number_format($row12['NewBalance'], 2); ?></td>
+        // interest (TransactionTypeID=8)
+        $stmtInt = $conn->prepare(
+          "SELECT SUM(Amount) AS sumAmt 
+           FROM tblmemberaccounts 
+           WHERE memberID=? AND TransactionTypeID=8 
+             AND TransactionDate BETWEEN ? AND ?"
+        );
+        $stmtInt->bind_param("iss",$ii,$d1,$d2);
+        $stmtInt->execute();
+        $rInt = $stmtInt->get_result();
+        $interest = $rInt->fetch_assoc()['sumAmt'] ?? 0;
+
+        // expenses (2,5,6,7)
+        $stmtExp = $conn->prepare(
+          "SELECT SUM(Amount) AS sumAmt 
+           FROM tblmemberaccounts 
+           WHERE memberID=? AND TransactionTypeID IN (2,5,6,7) 
+             AND TransactionDate BETWEEN ? AND ?"
+        );
+        $stmtExp->bind_param("iss",$ii,$d1,$d2);
+        $stmtExp->execute();
+        $rExp = $stmtExp->get_result();
+        $expenses = $rExp->fetch_assoc()['sumAmt'] ?? 0;
+
+        // payments (3,4)
+        $stmtPay = $conn->prepare(
+          "SELECT SUM(Amount) AS sumAmt 
+           FROM tblmemberaccounts 
+           WHERE memberID=? AND TransactionTypeID IN (3,4) 
+             AND TransactionDate BETWEEN ? AND ?"
+        );
+        $stmtPay->bind_param("iss",$ii,$d1,$d2);
+        $stmtPay->execute();
+        $rPay = $stmtPay->get_result();
+        $payments = $rPay->fetch_assoc()['sumAmt'] ?? 0;
+
+        // latest balance at period end
+        $stmtBal = $conn->prepare(
+          "SELECT NewBalance 
+           FROM tblmemberaccounts 
+           WHERE memberID=? AND TransactionDate<=? 
+           ORDER BY TransactionDate DESC LIMIT 1"
+        );
+        $stmtBal->bind_param("is",$ii,$d2);
+        $stmtBal->execute();
+        $rBal = $stmtBal->get_result();
+        $latestBal = $rBal->num_rows ? $rBal->fetch_assoc()['NewBalance'] : 0;
+        ?>
+        <td><?php echo number_format($initAmt,2); ?></td>
+        <td><?php echo number_format($interest,2); ?></td>
+        <td><?php echo number_format($expenses,2); ?></td>
+        <td><?php echo number_format($payments,2); ?></td>
+        <td><?php echo number_format($latestBal,2); ?></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 <?php
-							}
 						    
-						}else{
-								?>
-								<td scope="col" style="font-weight: bold;"><?php echo "No Balance";?></td>
-								<?php	
-							}
-?>		
-				</tr>
-				<tr style="text-align: center; background: white; color: black;">
-                    <th scope="col" colspan="6">Account Summary   [<?php echo date('d-M-Y')?>]</th>
-                   
-                    </tr>	
-				
-					
-<?php
-
-
-}    
-
-
-    
-    
-$stmt12 = $conn->prepare("SELECT SUM(`Amount`) AS `TT3` from `tblmemberaccounts` where  TransactionTypeID = '8' AND (`TransactionDate` BETWEEN '$d1' AND '$d2') AND memberID = '$ii' ");
-						$stmt12->execute();
-						$result12 = $stmt12->get_result();
-						if ($result12->num_rows > 0) {
-						    while($row12 = $result12->fetch_assoc()) {
-						  // output data of each row
-						 // $sum = $sum + $row12['TT3'] ;
-						 ?>
-
-                  
-                   
-                    <tr>
-                    <th scope="col" colspan="3" style="vertical-align: top;">Income Earned</th>
-                    <td scope="col" colspan="3" style="text-align: right;"><?php echo "E ". number_format($row12['TT3'], 2); ?></td>
-                    </tr>
-
- <?php	}
-	?>
-
-<?php	} else {
-echo "No Results";
-}    
- 
-
-
-    
-$stmt12 = $conn->prepare("SELECT SUM(`Amount`) AS `TT3` from `tblmemberaccounts` where  TransactionTypeID IN ('2','5', '6','7' ) AND (`TransactionDate` BETWEEN '$d1' AND '$d2') AND memberID = '$ii' ");
-						$stmt12->execute();
-						$result12 = $stmt12->get_result();
-						if ($result12->num_rows > 0) {
+						    
+}
+						
+} else {
+  header('location: ./');
+}
 						    while($row12 = $result12->fetch_assoc()) {
 						  // output data of each row
 						 // $sum = $sum + $row12['TT3'] ;
@@ -214,17 +214,6 @@ echo "0 results";	}
   <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
 
 <?php						
-
-
-						    
-						    
-}
-						
-} else {
-  header('location: ./');
-}
-
-
 
 
 
