ad95a13506caa57e45262d16aebff807913c5277
Siboniso Sibandze <sibandzesiboniso0@gmail.com>
2026-01-07 15:23:48 +0200

    Comment out server connection initialization in connection.php for security reasons

--- scripts/connection.php
+++ scripts/connection.php
@@ -1,10 +1,10 @@
 <?php
-//$servername = "srv1212.hstgr.io";
+$servername = "srv1212.hstgr.io";
 $username = "u747325399_fair2";
 $password = "2Jznf67RS=";
 $dbname   = 'u747325399_fair2';
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
