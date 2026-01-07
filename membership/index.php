701134b5f929edf81ea0053f017491bdcd14c645
Siboniso Sibandze <sibandzesiboniso0@gmail.com>
2026-01-07 15:17:07 +0200

    Update page title in index.php for clarity in members view

--- membership/index.php
+++ membership/index.php
@@ -13,7 +13,7 @@ require_once '../scripts/connection.php';
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>All Members</title>
+  <title>All Members View</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 <script src=" https://code.jquery.com/jquery-3.5.1.js"></script>
