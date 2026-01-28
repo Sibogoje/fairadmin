d4040ef2ec7ddabf86ba37660e8ddfae9d930af9
Siboniso Sibandze <81348382+Sibogoje@users.noreply.github.com>
2026-01-28 04:05:03 +0200

    Remove database connection logicll
    Remove database connection code from connection.php

--- scripts/connection.php
+++ scripts/connection.php
@@ -1,18 +1,1 @@
-<?php
-$servername = "148.163.121.29";
-$username = "dxtustit_fair2";
-$password = "Fairline@151022";
-$dbname   = 'dxtustit_fair2';
 
-// Make mysqli throw exceptions so we can catch them
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-try {
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $conn->set_charset('utf8mb4');
-} catch (mysqli_sql_exception $e) {
-    // Log the detailed error for debugging and return a generic message to the browser
-    error_log("DB connect error: " . $e->getMessage());
-    http_response_code(500);
-    die("Database connection error. Please try again later.");
-}
-?>
