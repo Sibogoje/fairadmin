935f74486d7b7938b5ed6cb96eda60538a10c66b
Siboniso Sibandze <sibandzesiboniso0@gmail.com>
2026-01-28 22:21:29 +0200

    Update favicon links in index.php to use local logo.png

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ require_once 'scripts/connection.php';
 
 
   <!-- Favicons -->
-  <link href="https://fairlife.grinpath.com/" rel="icon">
-  <link href="https://fairlife.grinpath.com/" rel="apple-touch-icon">
+  <link href="logo.png" rel="icon">
+  <link href="logo.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
